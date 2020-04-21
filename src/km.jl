167a54627d0417faed4aebd8e45257c8db1588ca
Antonello Lobianco <antonello@lobianco.org>
2020-04-21 10:21:16 +0200

    Moved all source code under src and separated initRepresentatives() from the K-Mean and K-Medoids algorithms

--- src/km.jl
+++ src/km.jl
@@ -6,19 +6,83 @@ using Distributions
 sterling(n::BigInt,k::BigInt) = (1/factorial(k)) * sum((-1)^i * binomial(k,i)* (k-i)^n for i in 0:k)
 sterling(n::Int64,k::Int64) = sterling(BigInt(n),BigInt(k))
 
+
+
+"""
+  initRepresentatives(X,K;initStrategy,Z₀))
+
+Initialisate the representatives for a K-Mean or K-Medoids algorithm
+
+# Parameters:
+* `X`: a (N x D) data to clusterise
+* `K`: Number of cluster wonted
+* `initStrategy`: Wheter to select the initial representative vectors:
+  * `random`: randomly in the X space
+  * `grid`: using a grid approach [default]
+  * `shuffle`: selecting randomly within the available points
+  * `given`: using a provided set of initial representatives provided in the `Z₀` parameter
+ * `Z₀`: Provided (K x D) matrix of initial representatives (used only together with the `given` initStrategy) [default: `nothing`]
+
+# Returns:
+* A (K x D) matrix of initial representatives
+
+# Example:
+```julia
+julia> Z₀ = initRepresentatives([1 10.5;1.5 10.8; 1.8 8; 1.7 15; 3.2 40; 3.6 32; 3.6 38],2,initStrategy="given",Z₀=[1.7 15; 3.6 40])
+```
+"""
+function initRepresentatives(X,K;initStrategy="grid",Z₀=nothing)
+    (N,D) = size(X)
+    # Random choice of initial representative vectors (any point, not just in X!)
+    minX = minimum(X,dims=1)
+    maxX = maximum(X,dims=1)
+    Z = zeros(K,D)
+    if initStrategy == "random"
+        for i in 1:K
+            for j in 1:D
+                Z[i,j] = rand(Uniform(minX[j],maxX[j]))
+            end
+        end
+    elseif initStrategy == "grid"
+        for d in 1:D
+                Z[:,d] = collect(range(minX[d], stop=maxX[d], length=K))
+        end
+    elseif initStrategy == "given"
+        if isnothing(Z₀) error("With the `given` strategy you need to provide the initial set of representatives in the Z₀ parameter.") end
+        for d in 1:D
+                Z = Z₀
+        end
+    elseif initStrategy == "shuffle"
+        for d in 1:D
+            zIdx = shuffle(1:size(X)[1])[1:K]
+            Z = X[zIdx, :]
+        end
+    else
+        error("initStrategy \"$initStrategy\" not implemented")
+    end
+    return Z
+end
+
+
 # Basic K-Means Algorithm (Lecture/segment 13.7 of https://www.edx.org/course/machine-learning-with-python-from-linear-models-to)
 
 """
-  kmean(X,K)
+  kmean(X,K,initStrategy)
 
 Compute K-Mean algorithm to identify K clusters of X using Euclidean distance
 
 # Parameters:
-* `X`: a (n x d) data to clusterise
+* `X`: a (N x D) data to clusterise
 * `K`: Number of cluster wonted
+* `initStrategy`: Wheter to select the initial representative vectors:
+  * `random`: randomly in the X space
+  * `grid`: using a grid approach [default]
+  * `shuffle`: selecting randomly within the available points
+  * `given`: using a provided set of initial representatives provided in the `Z₀` parameter
+ * `Z₀`: Provided (K x D) matrix of initial representatives (used only together with the `given` initStrategy) [default: `nothing`]
 
 # Returns:
-* A vector of size n of ids of the clusters associated to each point
+* A tuple of two items, the first one being a vector of size N of ids of the clusters associated to each point and the second one the (K x D) matrix of representatives
 
 # Notes:
 * Some returned clusters could be empty
@@ -28,24 +92,19 @@ Compute K-Mean algorithm to identify K clusters of X using Euclidean distance
 julia> clIdx = kmean([1 10.5;1.5 10.8; 1.8 8; 1.7 15; 3.2 40; 3.6 32; 3.6 38],2)
 ```
 """
-function kmean(X,K)
-    (n,d) = size(X)
+function kmean(X,K;initStrategy="grid",Z₀=nothing)
+    (N,D) = size(X)
     # Random choice of initial representative vectors (any point, not just in X!)
     minX = minimum(X,dims=1)
     maxX = maximum(X,dims=1)
-    Z₀ = zeros(K,d)
-    for i in 1:K
-        for j in 1:d
-            Z₀[i,j] = rand(Uniform(minX[j],maxX[j]))
-        end
-    end
-    Z = Z₀
-    cIdx_prev = zeros(Int64,n)
+    Z₀ = initRepresentatives(X,K,initStrategy=initStrategy,Z₀=Z₀)
+    Z  = Z₀
+    cIdx_prev = zeros(Int64,N)
 
     # Looping
     while true
         # Determining the constituency of each cluster
-        cIdx      = zeros(Int64,n)
+        cIdx      = zeros(Int64,N)
         for (i,x) in enumerate(eachrow(X))
             cost = Inf
             for (j,z) in enumerate(eachrow(Z))
@@ -71,6 +130,18 @@ function kmean(X,K)
     end
 end
 
+X = [1 10.5;1.5 10.8; 1.8 8; 1.7 15; 3.2 40; 3.6 32; 3.6 38]
+K = 3
+(N,D) = size(X)
+# Random choice of initial representative vectors (any point, not just in X!)
+minX = minimum(X,dims=1)
+maxX = maximum(X,dims=1)
+Z₀ = zeros(K,D)
+for d in 1:D
+        Z₀[:,d] = collect(range(minX[d], stop=maxX[d], length=K))
+end
+
+
 # Basic K-Medoids Algorithm (Lecture/segment 14.3 of https://www.edx.org/course/machine-learning-with-python-from-linear-models-to)
 
 
@@ -82,7 +153,7 @@ cos_distance(x,y) = dot(x,y)/(norm(x)*norm(y))
 
 
 """
-  kmedoids(X,K;dist)
+  kmedoids(X,K;dist,initStrategy,Z₀)
 
 Compute K-Medoids algorithm to identify K clusters of X using distance definition `dist`
 
@@ -90,6 +161,12 @@ Compute K-Medoids algorithm to identify K clusters of X using distance definitio
 * `X`: a (n x d) data to clusterise
 * `K`: Number of cluster wonted
 * `dist`: Function to employ as distance (must accept two vectors). Default to squared Euclidean.
+* `initStrategy`: Wheter to select the initial representative vectors:
+  * `random`: randomly in the X space
+  * `grid`: using a grid approach
+  * `shuffle`: selecting randomly within the available points [default]
+  * `given`: using a provided set of initial representatives provided in the `Z₀` parameter
+ * `Z₀`: Provided (K x D) matrix of initial representatives (used only together with the `given` initStrategy) [default: `nothing`]
 
 # Returns:
 * A vector of size n of ids of the clusters associated to each point
@@ -102,11 +179,10 @@ Compute K-Medoids algorithm to identify K clusters of X using distance definitio
 julia> clIdx = kmedoids([1 10.5;1.5 10.8; 1.8 8; 1.7 15; 3.2 40; 3.6 32; 3.6 38],2,dist = (x,y) -> norm(x-y)^2)
 ```
 """
-function kmedoids(X,K;dist=(x,y) -> norm(x-y)^2)
+function kmedoids(X,K;dist=(x,y) -> norm(x-y)^2,initStrategy="shuffle",Z₀=nothing)
     (n,d) = size(X)
     # Random choice of initial representative vectors
-    zIdx = shuffle(1:size(X)[1])[1:K]
-    Z₀ = X[zIdx, :]
+    Z₀ = initRepresentatives(X,K,initStrategy=initStrategy,Z₀=Z₀)
     Z = Z₀
     cIdx_prev = zeros(Int64,n)
 
@@ -154,3 +230,4 @@ function kmedoids(X,K;dist=(x,y) -> norm(x-y)^2)
     end
 end
 
+clIdx = kmedoids([1 10.5;1.5 10.8; 1.8 8; 1.7 15; 3.2 40; 3.6 32; 3.6 38],2,initStrategy="grid",Z₀=[3.6 32; 3.6 38])
