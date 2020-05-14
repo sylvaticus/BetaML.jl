ceb2e0c6764d1896b0bab86b9bf70cf0ef96ee59
Antonello Lobianco <antonello@lobianco.org>
2020-05-14 10:31:38 +0200

    Generalised neural network algorithm and added all support functions to a utilities file
    Actions are now delegated to individual layers, so one can create new layers.
    Two default type of layers are available: FullyConnectedLayer and NoBiasLayer.
    
    All utilities and shared functions are now in utilities.jl file.

--- src/nn.jl
+++ src/nn.jl
@@ -8,59 +8,218 @@ Neural Network algorithms
 - [Demonstrative live notebook](https://mybinder.org/v2/gh/sylvaticus/lmlj.jl/master?filepath=notebooks%2Fnn.ipynb) (temporary personal online computational environment on myBinder) - it can takes minutes to start with!
 - Theory based on [MITx 6.86x - Machine Learning with Python: from Linear Models to Deep Learning](https://github.com/sylvaticus/MITx_6.86x) ([Unit 3](https://github.com/sylvaticus/MITx_6.86x/blob/master/Unit%2003%20-%20Neural%20networks/Unit%2003%20-%20Neural%20networks.md))
 - New to Julia? [A concise Julia tutorial](https://github.com/sylvaticus/juliatutorial) - [Julia Quick Syntax Reference book](https://julia-book.com)
+
+FullyConnectedLayer and NoBiasLayer are already implemented and one can choose
+them with predefined activation functions or provide your own (including its derivative!)
+
+Alternativly you can implement your own layers.
+Each user-implemented layer must define the following methods:
+
+forward(layer,I) --> O
+- Predict the output of the layer given the input
+
+backward(layer,dϵ/dI^(l+1),I^l) --> dϵ/dI^l
+- return the evaluated derivative of the error for the input given (a) the derivative of the
+error with respect to the input of the next layer and (b) the input to this layer
+
+getW(layer) --> tuple
+- return the current weigths as tuple of matrices. Is is up to you to decide how
+to organise this tuple, as long you are consistent with the getDW() and setW() functions
+
+getDW(layer,dϵ/dI^(l+1),I^l) --> tuple
+- return the evaluated current weigths as tuple of matrices given (a) the derivative of the
+error with respect to the input of the next layer and (b) the input to this layer
+
+setW!(layer, tuple)
+- update the weights of the layer with the new values given
+
 """
 
 # ==================================
-# Neural Network Class
+# Neural Network Library
 # ==================================
+
+include(joinpath(@__DIR__,"utilities.jl"))
+
+
 
 using Random
 
 ## Some utility functions..
-import Base.reshape
-"""
-reshape(myNumber, dims..) - Reshape a number as a n dimensional Array
-"""
-function reshape(x::T, dims...) where {T <: Number}
-   x = [x]
-   reshape(x,dims)
-end
-function makeColVector(x::T) where {T <: Number}
-    return [x]
-end
-function makeColVector(x::T) where {T <: AbstractArray}
-    reshape(x,length(x))
-end
-function makeRowVector(x::T) where {T <: Number}
-    return [x]'
-end
-function makeRowVector(x::T) where {T <: AbstractArray}
-    reshape(x,1,length(x))
-end
 
 
+abstract type Layer end
+
+# ------------------------------------------------------------------------------
+# Provide default FullyConnectedLayer and NoBiasLayer
+
 """
-   Layer
+   FullyConnectedLayer
 
 Representation of a layer in the network
 
 # Fields:
-* `w`:  Weigths matrix with respect to the input from previous layer or data (n pr. layer x n)
+* `w`:  Weigths matrix with respect to the input from previous layer or data (n x n pr. layer)
 * `wb`: Biases (n)
 * `f`:  Activation function
 * `df`: Derivative of the activation function
 """
-mutable struct Layer
+mutable struct FullyConnectedLayer <: Layer
      w::Array{Float64,2}
      wb::Array{Float64,1}
      f::Function
      df::Function
+     """
+        FullyConnectedLayer(f,df,n,nₗ;w,wb)
+
+     Instantiate a new FullyConnectedLayer
+
+     Parameters:
+     * `f`:  Activation function
+     * `df`: Derivative of the activation function
+     * `nₗ`: Number of nodes of the previous layer
+     * `n`:  Number of nodes
+     * `w`:  Initial weigths with respect to input [default: `rand(n,nₗ)`]
+     * `wb`: Initial weigths with respect to bias [default: `rand(n)`]
+
+     """
+     function FullyConnectedLayer(f,df,nₗ,n;w=rand(n,nₗ),wb=rand(n))
+         # To be sure w is a matrix and wb a column vector..
+         w  = reshape(w,n,nₗ)
+         wb = reshape(wb,n)
+         return new(w,wb,f,df)
+     end
 end
 
 """
-   FNN
+   NoBiasLayer
 
-Representation of a Forward Neural Network
+Representation of a layer without bias in the network
+
+# Fields:
+* `w`:  Weigths matrix with respect to the input from previous layer or data (n x n pr. layer)
+* `f`:  Activation function
+* `df`: Derivative of the activation function
+"""
+mutable struct NoBiasLayer <: Layer
+     w::Array{Float64,2}
+     f::Function
+     df::Function
+     """
+        NoBiasLayer(f,df,nₗ,n;w)
+
+     Instantiate a new NoBiasLayer
+
+     Parameters:
+     *
+     * `f`:  Activation function
+     * `df`: Derivative of the activation function
+     * `nₗ`: Number of nodes of the previous layer
+     * `n`:  Number of nodes
+     * `w`:  Initial weigths with respect to input [default: `rand(n,nₗ)`]
+     """
+     function NoBiasLayer(f,df,nₗ,n;w=rand(n,nₗ))
+         # To be sure w is a matrix and wb a column vector..
+         w  = reshape(w,n,nₗ)
+         return new(w,f,df)
+     end
+end
+
+"""
+   forward(layer,x)
+
+Layer prediction
+
+# Parameters:
+* `layer`:  Worker layer
+* `x`:      Input to the layer
+"""
+function forward(layer::FullyConnectedLayer,x)
+  return layer.f.(layer.w * x + layer.wb)
+end
+"""
+   forward(layer,x)
+
+Layer prediction of a single data point
+
+# Parameters:
+* `layer`:  Worker layer
+* `x`:      Input to the layer
+"""
+function forward(layer::NoBiasLayer,x)
+  return layer.f.(layer.w * x)
+end
+
+
+function backward(layer::FullyConnectedLayer,nextGradient,x)
+   z = layer.w * x + layer.wb
+   dϵ_dz = layer.df.(z) .* nextGradient
+   dϵ_dI = layer.w' * dϵ_dz
+end
+
+function backward(layer::NoBiasLayer,nextGradient,x)
+   z = layer.w * x
+   dϵ_dz = layer.df.(z) .* nextGradient
+   dϵ_dI = layer.w' * dϵ_dz
+end
+
+"""
+   getW(layer)
+
+Retrieve current weigthts
+
+# Parameters:
+* `layer`: Worker layer
+
+# Return:
+* The output is a tuples of the layer's input weigths and bias weigths
+"""
+function getW(layer::FullyConnectedLayer)
+  return (layer.w,layer.wb)
+end
+function getW(layer::NoBiasLayer)
+  return (layer.w,)
+end
+
+function getDw(layer::FullyConnectedLayer,nextGradient,x)
+   z      = layer.w * x + layer.wb
+   dϵ_dz  = layer.df.(z) .* nextGradient
+   dϵ_dw  = dϵ_dz * x'
+   dϵ_dwb = dϵ_dz
+   return (dϵ_dw,dϵ_dwb)
+end
+
+function getDw(layer::NoBiasLayer,nextGradient,x)
+   z      = layer.w * x
+   dϵ_dz  = layer.df.(z) .* nextGradient
+   dϵ_dw  = dϵ_dz * x'
+   return (dϵ_dw,)
+end
+
+"""
+   setW!(layer,w)
+
+Set weigths of the layer
+
+# Parameters:
+* `layer`: Worker layer
+* `w`:   The new weights to set (tuple)
+"""
+function setW!(layer::FullyConnectedLayer,w)
+   layer.w = w[1]
+   layer.wb = w[2]
+end
+function setW!(layer::NoBiasLayer,w)
+   layer.w = w[1]
+end
+
+
+# ------------------------------------------------------------------------------
+# NN-related functions
+"""
+   NN
+
+Representation of a Neural Network
 
 # Fields:
 * `layers`:  Array of layers objects
@@ -68,32 +227,11 @@ Representation of a Forward Neural Network
 * `dcf`:     Derivative of the cost function
 * `trained`: Control flag for trained networks
 """
-mutable struct FNN
+mutable struct NN
     layers::Array{Layer,1}
     cf::Function
     dcf::Function
     trained::Bool
-end
-
-"""
-   buildLayer(f,df,n,nₗ;w,wb)
-
-Instantiate a new layer
-
-Parameters:
-* `f`:  Activation function
-* `df`: Derivative of the activation function
-* `n`:  Number of nodes
-* `nₗ`: Number of nodes of the previous layer
-* `w`:  Initial weigths with respect to input [default: `rand(nₗ,n)`]
-* `wb`: Initial weigths with respect to bias [default: `rand(n)`]
-
-"""
-function buildLayer(f,df,n,nₗ;w=rand(nₗ,n),wb=rand(n))
-    # To be sure w is a matrix and wb a column vector..
-    w  = reshape(w,nₗ,n)
-    wb = reshape(wb,n)
-    return Layer(w,wb,f,df)
 end
 
 """
@@ -111,55 +249,43 @@ Parameters:
 derivative should always expect y and ŷ as column vectors.
 """
 function buildNetwork(layers,cf,dcf)
-    return FNN(layers,cf,dcf,false)
+    return NN(layers,cf,dcf,false)
 end
 
-"""
-   predict(layer,x)
-
-Layer prediction of a single data point
-
-# Parameters:
-* `layer`:  Worker layer
-* `x`:      Input to the layer
-"""
-function predict(layer::Layer,x)
-  return layer.f.((reshape(x,1,length(x))*layer.w)' + layer.wb)
-end
 
 """
-   predict(fnn,x)
+   predict(nn,x)
 
 Network prediction of a single data point
 
 # Parameters:
-* `fnn`:  Worker network
-* `x`:    Input to the network
+* `nn`:  Worker network
+* `x`:   Input to the network
 """
-function predict(fnn::FNN,x)
+function predict(nn::NN,x)
     makeColVector(x)
     values = x
-    for l in fnn.layers
-        values = predict(l,values)
+    for l in nn.layers
+        values = forward(l,values)
     end
     return values
 end
 
 """
-   error(fnn,x,y)
+   error(nn,x,y)
 
 Compute network loss on a single data point
 
 # Parameters:
-* `fnn`: Worker network
+* `nn`: Worker network
 * `x`:   Input to the network
 * `y`:   Label input
 """
-function error(fnn::FNN,x,y)
+function error(nn::NN,x,y)
     x = makeColVector(x)
     y = makeColVector(y)
-    ŷ = predict(fnn,x)
-    return fnn.cf(ŷ,y)
+    ŷ = predict(nn,x)
+    return nn.cf(ŷ,y)
 end
 
 """
@@ -169,115 +295,122 @@ Compute avg. network loss on a test set
 
 # Parameters:
 * `fnn`: Worker network
-* `x`:   Input to the network (n x d)
+* `x`:   Input to the network (n) or (n x d)
 * `y`:   Label input (n) or (n x d)
 """
-function errors(fnn::FNN,x,y)
-    fnn.trained ? "" : @warn "Seems you are trying to test a neural network that has not been tested. Use first `test!(rnn,x,y)`"
+function errors(nn::NN,x,y)
+    x = makeMatrix(x)
+    y = makeMatrix(y)
+    nn.trained ? "" : @warn "Seems you are trying to test a neural network that has not been tested. Use first `train!(nn,x,y)`"
     ϵ = 0
     for i in 1:size(x)[1]
         xᵢ = x[i,:]'
         yᵢ = y[i,:]'
-        ϵ += error(fnn,xᵢ,yᵢ)
+        ϵ += error(nn,xᵢ,yᵢ)
     end
     return ϵ/size(x)[1]
 end
 
 """
-   getW(fnn)
+   getW(nn)
 
 Retrieve current weigthts
 
 # Parameters:
-* `fnn`: Worker network
+* `nn`: Worker network
 
 # Notes:
 * The output is a vector of tuples of each layer's input weigths and bias weigths
 """
 function getW(fnn)
-  w = Tuple{Array{Float64,2},Array{Float64,1}}[]
+  w = Tuple[]
   for l in fnn.layers
-      push!(w,(l.w,l.wb))
+      push!(w,getW(l))
   end
   return w
 end
 
+
 """
-   getDW(fnn,x,y)
+   getDw(nn,x,y)
 
 Retrieve the current gradient of the weigthts (i.e. derivative of the cost with respect to the weigths)
 
 # Parameters:
-* `fnn`: Worker network
-* `x`:   Input to the network
-* `y`:   Label input
+* `nn`: Worker network
+* `x`:   Input to the network (d,1)
+* `y`:   Label input (d,1)
 
 #Notes:
 * The output is a vector of tuples of each layer's input weigths and bias weigths
 """
-function getDW(fnn,x,y)
+function getDw(nn,x,y)
+
   x = makeColVector(x)
   y = makeColVector(y)
-  lz = Array{Float64,1}[]
-  lo = Array{Float64,1}[]
-  dW = Tuple{Array{Float64,2},Array{Float64,1}}[]
 
-  push!(lz,x)
-  push!(lo,x)
+  nLayers = length(nn.layers)
 
-  for l in fnn.layers
-      x = lo[end]
-      z = dropdims((reshape(x,1,length(x))*l.w)' + l.wb,dims=2)
-      o = l.f.(z)
-      push!(lz, z)
-      push!(lo, o)
+  # Stap 1: Forward pass
+  forwardStack = Array{Float64,1}[]
+  push!(forwardStack,x)
+  for l in nn.layers
+      push!(forwardStack, forward(l,forwardStack[end]))
   end
-  dc = fnn.dcf(lo[end],y)
-  δ = dc # derivative of the cost function with respect to the layer output
 
-  # backpropagation step
-  for lidx in length(fnn.layers):-1:1
-     l = fnn.layers[lidx]
-     # Note that lz and lo vectors includes x, so the second layer is the third element in the vector
-     dwb = l.df.(lz[lidx+1]) .* δ # derivative with respect to the layer biases
-     dw = lo[lidx] * dwb'         # derivative with respect to the layer input weigths
-     push!(dW,(dw,dwb))
-     # Computing derivatives of the cost function with respect of the output of the previous layer
-     δ = l.w * dwb
+  # Step 2: Backpropagation pass
+  backwardStack = Array{Float64,1}[]
+  push!(backwardStack,nn.dcf(forwardStack[end],y)) # adding d€_dHatY
+  for lidx in nLayers:-1:1
+     l = nn.layers[lidx]
+     d€_do = backward(l,backwardStack[end],forwardStack[lidx])
+     push!(backwardStack,d€_do)
   end
-  return dW[end:-1:1] # reversing it, to start from the first layer
+  backwardStack = backwardStack[end:-1:1] # reversing it,
+
+  # Step 3: Computing gradient of weigths
+  dWs = Tuple[]
+  for lidx in 1:nLayers
+     l = nn.layers[lidx]
+     dW = getDw(l,backwardStack[lidx+1],forwardStack[lidx])
+     push!(dWs,dW)
+  end
+
+  return dWs
 end
 
 """
-   updateWeights!(fnn,w)
+   setW!(nn,w)
 
 Update weigths of the network
 
 # Parameters:
-* `fnn`: Worker network
-* `w`:   The new weights to set
+* `nn`: Worker network
+* `w`:  The new weights to set
 """
-function updateWeights!(fnn,w)
-    for lidx in 1:length(fnn.layers)
-        fnn.layers[lidx].w = w[lidx][1]
-        fnn.layers[lidx].wb = w[lidx][2]
+function setW!(nn,w)
+    for lidx in 1:length(nn.layers)
+        setW!(nn.layers[lidx],w[lidx])
     end
 end
 
+
 """
-   train!(fnn,x,y;epochs,η,rshuffle)
+   train!(nn,x,y;epochs,η,rshuffle)
 
 Train a fnn with the given x,y data
 
 # Parameters:
-* `fnn`:      Worker network
+* `nn`:      Worker network
 * `x`:        Training input to the network (records x dimensions)
-* `y`:        Label input (records)
+* `y`:        Label input (records x dimensions)
 * `epochs`:   Number of passages over the training set [def = `1000`]
 * `η`:        Learning rate. If not provided 1/(1+epoch) is used [def = `nothing`]
 * `rshuffle`: Whether to random shuffle the training set at each epoch [def = `true`]
 """
-function train!(fnn,x,y;epochs=1000, η=nothing, rshuffle=true)
+function train!(nn,x,y;epochs=1000, η=nothing, rshuffle=true)
+    x = makeMatrix(x)
+    y = makeMatrix(y)
     logStep = Int64(ceil(epochs/100))
     dyn_η = η == nothing ? true : false
     for t in 1:epochs
@@ -291,56 +424,67 @@ function train!(fnn,x,y;epochs=1000, η=nothing, rshuffle=true)
         η = dyn_η ? 1/(1+t) : η
         for i in 1:size(x)[1]
             xᵢ = x[i,:]'
-            yᵢ = makeColVector(y[i])
-            w  = getW(fnn)
-            dW = getDW(fnn,xᵢ,yᵢ)
-            for (lidx,l) in enumerate(fnn.layers)
-                l.w  = l.w -  η .* dW[lidx][1]
-                l.wb = l.wb - η .* dW[lidx][2]
+            yᵢ = y[i,:]'
+            W  = getW(nn)
+            dW = getDw(nn,xᵢ,yᵢ)
+            for (lidx,l) in enumerate(nn.layers)
+                oldW = W[lidx]
+                dw = dW[lidx]
+                newW = oldW .- η .* dw
+                setW!(l,newW)
             end
-            ϵ += error(fnn,xᵢ,yᵢ)
+            ϵ += error(nn,xᵢ,yᵢ)
         end
         (t % logStep == 0) || t == 1 || t == epochs ? println("Avg. error after epoch $t : $(ϵ/size(x)[1])") : ""
     end
-    fnn.trained = true
+    nn.trained = true
 end
-
 
 # ==================================
 # Specific implementation - FNN definition
 # ==================================
 
-# Defining the functions we fill use as activation function as well their derivatives
-# (yes, we could have used instead an automatic differentiation - AD - library..)
-relu(x)     = max(0,x)
-drelu(x)    = x <= 0 ? 0 : 1
-linearf(x)  = x
-dlinearf(x) = 1
-cost(ŷ,y)   = (1/2)*(y[1]-ŷ[1])^2
-dcost(ŷ,y)  = [- (y[1]-ŷ[1])]
-
-l1 = buildLayer(relu,drelu,3,2,w=[1 1 1;1 1 1],wb=[0,0,0])
-l2 = buildLayer(linearf,dlinearf,1,3,w=[1,1,1],wb=0)
-myfnn = buildNetwork([l1,l2],cost,dcost)
+l1 = FullyConnectedLayer(tanh,dtanh,2,3,w=[1 1; 1 1; 1 1], wb=[0 0 0])
+l2 = FullyConnectedLayer(tanh,dtanh,3,2, w=[1 1 1; 1 1 1], wb=[0 0])
+l3 = FullyConnectedLayer(linearf,dlinearf,2,1, w=[1 1], wb=[0])
+mynn = buildNetwork([l1,l2,l3],squaredCost,dSquaredCost)
 
 # ==================================
 # Usage of the FNN
 # ==================================
 
-xtrain = [2 1; 3 3; 4 5; 6 6]
-ytrain = [10,21,32,42]
-ytrain = [14,21,28,42]
-xtest  = [1 1; 2 2; 3 3; 5 5; 10 10]
-ytest  = [7,14,21,35,70]
+# ------------------
+xtrain = [0.1 0.2; 0.3 0.5; 0.4 0.1; 0.5 0.4; 0.7 0.9; 0.2 0.1]
+ytrain = [0.3; 0.8; 0.5; 0.9; 1.6; 0.3]
+xtest = [0.5 0.6; 0.14 0.2; 0.3 0.7]
+ytest = [1.1; 0.36; 1.0]
 
-train!(myfnn,xtrain,ytrain,epochs=10,η=0.001,rshuffle=false) # 1.86
-errors(myfnn,xtest,ytest) # 0.108
+train!(mynn,xtrain,ytrain,epochs=10000,η=0.01, rshuffle=false)
+errors(mynn,xtest,ytest) # 0.000196
+for (i,r) in enumerate(eachrow(xtest))
+  println("x: $r ŷ: $(predict(mynn,r)[1]) y: $(ytest[i])")
+end
 
-dtanh(x)    = 1-tanh(x)^2
-l1 = buildLayer(tanh,dtanh,3,2)
-l2 = buildLayer(linearf,dlinearf,1,3)
-myfnn2 = buildNetwork([l1,l2],cost,dcost)
+# ==================================
+# Individual components debugging stuff
+# ==================================
 
-train!(myfnn2,xtrain,ytrain,epochs=10000,η=0.001,rshuffle=false) # 0.011
-errors(myfnn2,xtest,ytest) # 76.9
-```
+l1 = FullyConnectedLayer(relu,drelu,2,3,w=[1 2; -1 -2; 3 -3],wb=[1,-1,0])
+l2 = NoBiasLayer(linearf,dlinearf,3,2,w=[1 2 3; -1 -2 -3])
+X = [3,1]
+Y = [10,0]
+o1 = forward(l1,X)
+o2 = forward(l2,o1)
+ϵ = squaredCost(o2,Y)
+d€_do2 = dSquaredCost(o2,Y)
+d€_do1 = backward(l2,d€_do2,o1)
+d€_dX = backward(l1,d€_do1,X)
+l1w = getW(l1)
+l2w = getW(l2)
+l2dw = getDw(l2,d€_do2,o1)
+l1dw = getDw(l1,d€_do1,X)
+setW!(l1,l1w)
+setW!(l2,l2w)
+mynn = buildNetwork([l1,l2],squaredCost,dSquaredCost)
+predict(mynn,X)
+ϵ2 = error(mynn,X,Y)
---
+++ src/utilities.jl
@@ -0,0 +1,65 @@
+
+###############################
+# Shared utilities functions ##
+###############################
+
+using LinearAlgebra
+# ------------------------------------------------------------------------------
+# Various reshaping functions
+
+import Base.reshape
+""" reshape(myNumber, dims..) - Reshape a number as a n dimensional Array """
+reshape(x::T, dims...) where {T <: Number} =   (x = [x]; reshape(x,dims) )
+makeColVector(x::T) where {T <: Number} =  [x]
+makeColVector(x::T) where {T <: AbstractArray} =  reshape(x,length(x))
+makeRowVector(x::T) where {T <: Number} = return [x]'
+makeRowVector(x::T) where {T <: AbstractArray} =  reshape(x,1,length(x))
+"""Transform an Array{T,1} in an Array{T,2} and leave unchanged Array{T,2}."""
+makeMatrix(x::Array) = ndims(x) == 1 ? reshape(x, (size(x)...,1)) : x
+
+# ------------------------------------------------------------------------------
+# Various neural network activation functions as well their derivatives
+
+relu(x)     = max(0,x)
+drelu(x)    = x <= 0 ? 0 : 1
+linearf(x)  = x
+dlinearf(x) = 1
+#tanh(x) already in Julia base
+dtanh(x)    = 1-tanh(x)^2
+sigmoid(x)  = 1/(1+exp(-x))
+dsigmoid(x) = exp(-x)*sigmoid(x)^2
+
+# ------------------------------------------------------------------------------
+# Various neural network loss functions as well their derivatives
+squaredCost(ŷ,y)   = (1/2)*norm(y - ŷ)^2
+dSquaredCost(ŷ,y)  = .- (y .- ŷ)
+
+# ------------------------------------------------------------------------------
+# Some common distance measures
+
+"""L1 norm distance (aka "Manhattan Distance")"""
+l1_distance(x,y)     = sum(abs.(x-y))
+"""Euclidean (L2) distance"""
+l2_distance(x,y)     = norm(x-y)
+"""Squared Euclidean (L2) distance"""
+l2²_distance(x,y)    = norm(x-y)^2
+"""Cosine distance"""
+cosine_distance(x,y) = dot(x,y)/(norm(x)*norm(y))
+
+# ------------------------------------------------------------------------------
+# Some common PDFs
+
+""" PDF of a multidimensional normal with no covariance and shared variance across dimensions"""
+normalFixedSd(x,μ,σ²)    = (1/(2π*σ²)^(length(x)/2)) * exp(-1/(2σ²)*norm(x-μ)^2)
+""" log-PDF of a multidimensional normal with no covariance and shared variance across dimensions"""
+logNormalFixedSd(x,μ,σ²) = - (length(x)/2) * log(2π*σ²)  -  norm(x-μ)^2/(2σ²)
+
+
+# ------------------------------------------------------------------------------
+# Other mathematical/computational functions
+
+""" LogSumExp for efficiently computing log(sum(exp.(x))) """
+myLSE(x) = maximum(x)+log(sum(exp.(x .- maximum(x))))
+""" Sterling number: number of partitions of a set of n elements in k sets """
+sterling(n::BigInt,k::BigInt) = (1/factorial(k)) * sum((-1)^i * binomial(k,i)* (k-i)^n for i in 0:k)
+sterling(n::Int64,k::Int64)   = sterling(BigInt(n),BigInt(k))
