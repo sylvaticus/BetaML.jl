a9e96f22039e160b8891ab2abcecca608bfc6ab9
Antonello Lobianco <antonello@lobianco.org>
2022-08-11 15:31:22 +0200

    Removed (needs work) testing on initial perceptron v2API attempt

--- test/Perceptron_tests.jl
+++ test/Perceptron_tests.jl
@@ -55,7 +55,7 @@ ŷtest  = predict(xtest,out.θ,out.θ₀,out.classes)
 @test ϵtrain  < 0.4
 @test ϵavg    < 0.4
 
-m = PerceptronClassic()
+#m = PerceptronClassic() TODO....
 
 
 # ==================================
